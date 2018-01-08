80ca668c723d5c73a052b73f201f04983b6a6101
giga0 <igor.s.bogdanovic@gmail.com>
2018-01-08 17:06:05 +0100

    long-read js at work

--- app/js_dev/blocks/long-read-main-fnc.js
+++ app/js_dev/blocks/long-read-main-fnc.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
 
 		var longReadContent = $('#long-read-content'),
 			longReadPanel = $('.long-read-panel'),
+			longReadPanelSecWithQuote = $('.long-read__sec--quote'),
+			longReadIntroHeading = $('.long-read__intro-heading'),
 
 			$windowWidth = $(window).width(),
 			$breakpoint = 750;
@@ -15,33 +17,122 @@ $(document).ready(function() {
 			$('html').css({ 'height': '100%', 'margin': '0' });
 			$('body').css({ 'height': '100%', 'margin': '0' });
 
-			var controller = new ScrollMagic.Controller();
+			var controller1 = new ScrollMagic.Controller();
+			var controller2 = new ScrollMagic.Controller({container: '.long-read__intro'});
+			//var controller3 = new ScrollMagic.Controller({container: '.long-read__sec--quote'});
 
 			var longReadContentLength = longReadContent.children().length;
 
-			for (var i = 1, j = 0; i < longReadContentLength; i++) {
-				var longReadPanelClass = 'lr-panel-' + j;
-				j =+ i;
-				longReadPanel.eq(i).addClass(longReadPanelClass);
-			}
+			// setting lr-panel-'No' classes to elements in html
+			// for (var i = 1, j = 0; i < longReadContentLength; i++) {
+			// 	var longReadPanelClass = 'lr-panel-' + j;
+			// 	j =+ i;
+			// 	longReadPanel.eq(i).addClass(longReadPanelClass);
+			// }
 
-			var wipeAnimation = new TimelineMax()
-			    .fromTo("section.lr-panel-0", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})
-			    .fromTo("section.lr-panel-1", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})
-			    .fromTo("section.lr-panel-2", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})
-			    .fromTo("section.lr-panel-3", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})
-			    .fromTo("section.lr-panel-4", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})
-			    .to("section.long-read-panel .lr-panel__content", 1, {y: "-100%", ease: Linear.easeNone});
+			// section wipes functionality
+			var wipeAnimation = new TimelineMax();
 
-			new ScrollMagic.Scene({
+			longReadPanel.each(function(index, elem){
+				var el = $(elem);
+
+				if (index > 0 && index < longReadContentLength) {
+					wipeAnimation.fromTo(elem, 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone});
+
+					if (el.children().hasClass('lr-panel__content')) {
+						wipeAnimation.to("section.long-read-panel .lr-panel__content", 1, {y: "-100%", ease: Linear.easeNone});
+					}
+				}
+			});
+
+			// scene for section wipes
+			var scene1 = new ScrollMagic.Scene({
 			        triggerElement: "#long-read-content",
 			        triggerHook: "onLeave",
-			        duration: "500%"
+			        duration: "600%"
 			    })
 			    .setPin("#long-read-content")
 			    .setTween(wipeAnimation)
-			    .addIndicators()
-			    .addTo(controller);
+			    //.addIndicators()
+			    .addTo(controller1);
+
+			// scene for moving intro header
+			var scene2 = new ScrollMagic.Scene({
+			        triggerElement: ".long-read__intro-txt",
+			        triggerHook: 0.53,
+			        duration: "53%"
+			    })
+			    .setTween(TweenMax.to('.long-read__intro-heading', 1, { top: "24vh" }))
+			    //.addIndicators()
+			    .addTo(controller2);
+
+			// scene for removing z-index on intro header so it doesn't have priority on other slides
+			var scene3 = new ScrollMagic.Scene({
+			        triggerElement: ".long-read__video-sec",
+			        triggerHook: 0.18
+			    })
+				.on("start end", function () {
+					longReadIntroHeading.toggleClass('z-index');
+				})
+			    //.addIndicators()
+			    .addTo(controller2);
+
+			// section quote functionality
+			var quoteAnimation = new TimelineMax();
+			var longReadPanelSecWithQuoteLength = longReadPanelSecWithQuote.length;
+			var scenesQuote = [];
+
+			longReadPanelSecWithQuote.each(function(index, elem){
+				var el = $(elem);
+				var quote = el.find('.long-read__sec__quote');
+				// console.log(el);
+				// console.log(longReadPanelSecWithQuoteLength);
+
+				// quoteAnimation.to(quote, 1, { left: 0 });
+
+
+				if (index < longReadPanelSecWithQuoteLength) {
+					
+					// console.log(elem);
+					// console.log(quote);
+					quoteAnimation.to(quote, 1, { left: 0 });
+
+					// scenesQuote.push(
+					// 	(new ScrollMagic.Scene({
+					//         triggerElement: elem,
+					//         triggerHook: 0.11,
+					//         duration: 0
+					//     })
+					//     //.setTween(new TimelineMax().to('.long-read__sec__quote', 1, { left: 0 }))
+					//     .setTween(quoteAnimation.to(quote, 1, { left: 0 });)
+					//     .addIndicators()
+					//     .addTo(controller1);)
+					// );
+
+					var scene4 = new ScrollMagic.Scene({
+				        triggerElement: elem,
+				        triggerHook: 0.11,
+				        duration: 0
+				    })
+				    //.setTween(new TimelineMax().to('.long-read__sec__quote', 1, { left: 0 }))
+				    .setTween(quoteAnimation)
+				    .addIndicators()
+				    .addTo(controller1);
+				}
+
+
+
+				// scene for quote transition
+
+
+				// if (index > 0 && index < longReadContentLength) {
+				// 	wipeAnimation.fromTo(elem, 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone});
+
+				// 	if (el.children().hasClass('lr-panel__content')) {
+				// 		wipeAnimation.to("section.long-read-panel .lr-panel__content", 1, {y: "-100%", ease: Linear.easeNone});
+				// 	}
+				// }
+			});
 
 		}
 
